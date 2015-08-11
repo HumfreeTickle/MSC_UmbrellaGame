fabe4086b9ac1d3b77ee8e8f16901f11883aca07
petercantwell <peter.cantwell016@gmail.com>
2015-08-11 10:20:19 +0100

    11 august

---
+++ TheUmbrellaGame/Assets/100101/_Environment/DropTheBridge.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropTheBridge : MonoBehaviour {
+
+	public bool drop;
+	public float minAngle;
+	public float maxAngle;
+	public float speed;
+	public Transform from;
+	public Transform to;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (drop){
+			//transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, Time.time * speed);
+ 			float angle = Mathf.LerpAngle (minAngle, maxAngle, Time.deltaTime * speed);
+			transform.eulerAngles = new Vector3(-112,0,0);
+		}
+	}
+}
