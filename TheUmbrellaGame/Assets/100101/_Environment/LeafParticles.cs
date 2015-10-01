87f094ffb57ad33037be88a646bbcf425d8b18a4
Stephen Larkin <stephenlarkin64@gmail.com>
2015-10-01 12:02:30 +0100

    Final Final push

---
+++ TheUmbrellaGame/Assets/100101/_Environment/LeafParticles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Environment
+{
+	[RequireComponent (typeof (MeshCollider))]
+
+	public class LeafParticles : MonoBehaviour
+	{
+		private ParticleSystem leafParticle;
+		public GameObject leafParticleSystem;
+		private Transform particleChild;
+
+		void Start ()
+		{
+			GetComponent<MeshCollider>().convex = true;
+			GetComponent<MeshCollider>().isTrigger = true;
+			if(transform.childCount == 0){
+				particleChild = Instantiate(leafParticleSystem, transform.position, Quaternion.identity) as Transform;
+				particleChild.parent = this.transform;
+			}
+
+			leafParticle = this.gameObject.transform.FindChild ("Leaf System").GetComponent<ParticleSystem>();
+		}
+	
+		void OnTriggerEnter (Collider col)
+		{
+			if (col.gameObject.tag == "Player") {
+				leafParticle.Play();
+			}
+		}
+
+		void OnTriggerExit(Collider col)
+		{
+			if (col.gameObject.tag == "Player") {
+				leafParticle.Stop();
+			}
+		}
+	}
+}
