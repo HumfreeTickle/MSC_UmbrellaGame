b78f71becd99d4c318e1fb703e6a40db4f2fa0ca
petercantwell <peter.cantwell016@gmail.com>
2015-12-03 10:49:57 +0000

    Dec3

---
+++ TheUmbrellaGame/Assets/WaterFallRainSFX.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFallRainSFX : MonoBehaviour {
+
+	private AudioClip Rain;
+	private AudioSource audio2;
+
+	// Use this for initialization
+	void Start () {
+	
+		audio2 = GetComponent<AudioSource>();
+		Rain = audio2.clip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.tag == "Player") {
+
+		
+			audio2.Play();
+		}
+	
+
+	}
+	void OnTriggerExit(Collider col){
+
+		if (col.gameObject.tag == "Player") {
+			
+
+			audio2.Stop();
+		}
+
+	}
+
+}
